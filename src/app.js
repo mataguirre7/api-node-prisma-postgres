ba5b71969e60566f6608ae76d60bb6cbb8312c19
mataguirre <mataguirre.dev@gmail.com>
2025-04-11 16:46:28 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ import app_router from "./routes/app.router.js";
 config();
 const app = express();
 const PORT = process.env.PORT;
-const api_url = '/api';
+const api_url = "/api";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(api_url, app_router);
 
+app.use((req, res) => {
+  res.redirect(api_url);
+});
+
 app.listen(PORT, () => {
-  console.log(`server initialized at http://localhost:${PORT}`);
+  console.log(`server initialized at http://localhost:${PORT}/api`);
 });
