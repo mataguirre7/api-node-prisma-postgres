17c80fd03293360779feab3f2ebbd31d98b83d1c
mataguirre <mataguirre.dev@outlook.com>
2025-05-01 01:16:57 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { config } from "dotenv";
 import app_router from "./routes/app.router.js";
+import cors from "cors";
 
 config();
 const app = express();
 const PORT = process.env.PORT;
 const api_url = "/api";
+
+app.use(
+  cors({
+    origin: ["http://localhost:4200"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
